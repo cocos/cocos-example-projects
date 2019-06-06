fe7ebf1d7f93abbfc29d85634a62bd734c9d77b5
Yun Hsiao Wu <inheritorofgalaxy@gmail.com>
2019-06-07 00:03:35 +0800

    sponza night

---
+++ demo01/assets/scripts/sponza.js
@@ -0,0 +1,28 @@
+import { _decorator, Component, CameraComponent } from "Cocos3D";
+const { ccclass, property } = _decorator;
+
+@ccclass("Sponza")
+export class Sponza extends Component {
+
+    @property
+    loopTime = 3600;
+
+    @property
+    maxIllum = 20000;
+
+    start () {
+        this.ambient = this.node.scene.renderScene.ambient;
+        this.halfLoopTime = this.loopTime * 0.5;
+        this.camera = this.node.getComponent(CameraComponent);
+        this.color = cc.color();
+    }
+
+    update (deltaTime) {
+        let illum = 0;
+        const t = cc.director.getTotalFrames() % this.loopTime;
+        if (t > this.halfLoopTime) illum = Math.sin((t - this.halfLoopTime) / this.halfLoopTime * Math.PI);
+        this.ambient.skyIllum = illum * this.maxIllum;
+        this.color.r = this.color.g = this.color.b = illum * 255;
+        this.camera.color = this.color;
+    }
+}
