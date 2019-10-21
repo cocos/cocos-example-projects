cbb85553c7e227c0cbf52ca8177b215863f5138e
Yun Hsiao Wu <inheritorofgalaxy@gmail.com>
2019-10-21 14:53:42 +0800

    update

--- demo02/assets/scripts/skin/SSS.js
+++ demo02/assets/scripts/skin/SSS.js
@@ -1,19 +1,22 @@
-import { _decorator, Component, ModelComponent, Vec4 } from "cc";
+import { _decorator, Component, ModelComponent, GFXType } from "cc";
 const { ccclass } = _decorator;
 
 @ccclass("SSS")
 export class SSS extends Component {
 
-    _material = null;
+    _handle = null;
 
     start () {
-        this._material = this.node.getComponent(ModelComponent).material;
+        const mat = this.node.getComponent(ModelComponent).material;
+        this._pass = mat.passes[0];
+        this._handle = this._pass.getHandle('pbrParams', 3, GFXType.FLOAT);
     }
 
     setSSSIntensity (e) {
-        let value = this._material.getProperty('pbrParams');
-        if (!value) { value = new Vec4(0.8, 0.6, 1, 1); }
-        value.w = e.progress;
-        this._material.setProperty('pbrParams', value);
+        this._pass.setUniform(this._handle, e.progress);
+    }
+
+    toggle (e) {
+        this.node.active = e.isChecked;
     }
 }
