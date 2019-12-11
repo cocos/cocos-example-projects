9caad8ea4999554fe61cab59f6fbb5a0c0da5f07
Yun Hsiao Wu <inheritorofgalaxy@gmail.com>
2019-12-11 10:47:23 +0800

    add lint hint

---
+++ demo02/assets/prefabs/light-hint/light-hint.ts
@@ -0,0 +1,24 @@
+import { _decorator, Component, CameraComponent, Material, ModelComponent, SpotLightComponent, SphereLightComponent, Vec2 } from "cc";
+const { ccclass } = _decorator;
+
+@ccclass("LightHint")
+export class LightHint extends Component {
+
+    private _material: Material = null;
+    private _camera: CameraComponent = null;
+
+    start () {
+        let light: SphereLightComponent | SpotLightComponent = this.node.parent.getComponent(SphereLightComponent);
+        if (!light) light = this.node.parent.getComponent(SpotLightComponent);
+        this._material = this.node.getComponent(ModelComponent).material;
+        this._camera = this.node.scene.getComponentInChildren(CameraComponent);
+        const size = light.size * 4;
+        this.node.setWorldScale(size, size, size);
+        this._material.setProperty('color', light.color);
+        this._material.setProperty('intensitySize', new Vec2(light.luminance, 0.4));
+    }
+
+    update () {
+        this.node.setWorldRotation(this._camera.node.worldRotation);
+    }
+}
