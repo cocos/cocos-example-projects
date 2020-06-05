8d2dbc20e4c71cab34c301e434f9845acc3d19a4
ChiaNing <sun.cloud.mo@gmail.com>
2020-06-05 17:33:27 +0800

    testlist bug fix

--- show-cases/assets/scripts/testlist/backbutton.ts
+++ show-cases/assets/scripts/testlist/backbutton.ts
@@ -79,7 +79,7 @@ export class BackButton extends Component {
         director.loadScene('testlist', function () {
             BackButton._sceneIndex = -1;
             BackButton.refreshButton();
-            BackButton._scrollNode = self.node.getParent().getChildByPath('Canvas/ScrollView') as Node;
+            BackButton._scrollNode = this.node.getParent().getChildByPath('Canvas/ScrollView') as Node;
             if (BackButton._scrollNode) {
                 BackButton._scrollCom = BackButton._scrollNode.getComponent(ScrollViewComponent);
                 BackButton._scrollCom._content.getComponent(LayoutComponent).updateLayout();
