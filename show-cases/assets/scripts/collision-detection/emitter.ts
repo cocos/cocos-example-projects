0661c3301408e115a5187ecf6b66ed3300b5efff
jk20012001 <jkregister@163.com>
2021-10-22 16:46:47 +0800

    Fix: collision-detection物体上覆盖着一层阴影

--- show-cases/assets/scripts/collision-detection/emitter.ts
+++ show-cases/assets/scripts/collision-detection/emitter.ts
@@ -57,9 +57,9 @@ export class Emitter extends Component {
       const hintModel = hint.addComponent(ModelComponent);
       const hintMat = new Material();
       hintMat.initialize({ effectName: 'builtin-standard' });
-      const hintcolor = this.color.clone();
-      hintcolor.a = 255;
-      hintMat.setProperty('albedo', hintcolor);
+      const hintColor = this.color.clone();
+      hintColor.a = 255;
+      hintMat.setProperty('albedo', hintColor);
       hintMat.setProperty('metallic', 0.1);
       hintModel.material = hintMat;
       hintModel.mesh = hintMesh;
