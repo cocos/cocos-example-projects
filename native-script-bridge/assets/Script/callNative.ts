316a8667a639de8f0c2f1e2e5447aa4b4e5df42b
timlyeee <timelyeee@gmail.com>
2021-12-24 16:21:16 +0800

    use jsb.EventHandler

--- native-script-bridge/assets/Script/callNative.ts
+++ native-script-bridge/assets/Script/callNative.ts
@@ -1,5 +1,5 @@
 
-import { _decorator, Component, Node, Label, color, Color, MeshRenderer, Canvas, UITransform, Light } from 'cc';
+import { _decorator, Component, Node, Label, color, Color, MeshRenderer, Canvas, UITransform, Light, View, jsEventHandler, JsEventHandler, Game } from 'cc';
 import { CC_LOG_DEBUG, MethodManager } from './MethodManager';
 const { ccclass, property } = _decorator;
 
@@ -13,21 +13,18 @@ export class CallNative extends Component {
     @property(Light)
     public lightToChange: Light | undefined;
     start() {
-        new MethodManager;
-        jsb.bridge.onNative = (methodName: string, arg1?: string | null) => {
-            CC_LOG_DEBUG("Trigger event for " + methodName + " is " + MethodManager.instance.applyMethod(methodName, arg1!));
-        };
         this.registerAllScriptEvent();
+
     }
 
     public registerAllScriptEvent() {
-        MethodManager.instance.addMethod("changeLabelContent", (usr: string) => {
+        jsb.jsEventHandler.addCallback("changeLabelContent", (usr: string) => {
             this.changeLabelContent(usr);
         });
-        MethodManager.instance.addMethod("changeLabelColor", () => {
+        jsb.jsEventHandler.addCallback("changeLabelColor", () => {
             this.changeLabelColor();
         });
-        MethodManager.instance.addMethod("changeLightColor", () => {
+        jsb.jsEventHandler.addCallback("changeLightColor", () => {
             this.changeLightColor();
         });
     }
@@ -35,8 +32,8 @@ export class CallNative extends Component {
     public changeLabelContent(user: string): void {
         CC_LOG_DEBUG("Hello " + user + " I'm K");
         this.labelForContent!.string = "Hello " + user + " ! I'm K";
-        
-        
+
+
     }
     public changeLabelColor(): void {
         this.labelForColor!.color = new Color("#B8F768");
@@ -47,16 +44,46 @@ export class CallNative extends Component {
     }
 
     //Button click event for SAY HELLO
-    public sayHelloBtn(){
-        jsb.bridge.sendToNative("requestLabelContent");
+    public sayHelloBtn() {
+        jsb.jsEventHandler.sendToNative("requestLabelContent");
     }
 
     //Button click event for CHANGE LABEL COLOR
-    public changeLabelColorBtn(){
-        jsb.bridge.sendToNative("requestLabelColor");
+    public changeLabelColorBtn() {
+        jsb.jsEventHandler.sendToNative("requestLabelColor");
     }
 
-    public changeLightColorBtn(){
-        jsb.bridge.sendToNative("requestBtnColor", "50");
+    public changeLightColorBtn() {
+        jsb.jsEventHandler.sendToNative("requestBtnColor", "50");
     }
+
+    private defaultEvent = "default";
+    private defaultEventCbCount = 0;
+    private myCbList: jsb.JsCallback[] = [];
+    public addOneCallback() {
+        var self = this;
+        var cb = (arg0: string) => {
+            console.log(`trigger time with for event ${this.defaultEvent} with arg0 ${arg0} and defaultEventCbCount ${self.defaultEventCbCount} `);
+        }
+        this.myCbList.push(cb);
+        jsb.jsEventHandler.addCallback(this.defaultEvent,cb)
+    }
+    private eventId = 0;
+    public add100Callback() {
+        var self = this;
+        for (var i = 0; i < 100; i++) {
+            jsb.jsEventHandler.addCallback(self.eventId.toString(), (arg0: string) => {
+                console.log(`add one hundred for event ${self.eventId} with callback id ${i}`);
+            })
+        }
+        this.eventId++;
+    }
+
+    public removeOneCallback() {
+        var self = this;
+        jsb.jsEventHandler.removeCallback(this.defaultEvent,this.myCbList[0]);
+
+    }
+
+
 }
--- native-script-bridge/native/engine/android/app/src/com/cocos/game/JsbBridgeTest.java
+++ native-script-bridge/native/engine/android/app/src/com/cocos/game/JsbBridgeTest.java
@@ -1,34 +1,23 @@
 //Script to add into com.cocos.game package, backup here
 package com.cocos.game;
 
+import com.cocos.lib.JsJavaEventHandler;
 import com.cocos.lib.JsbBridge;
 import java.util.HashMap;
 
 public class JsbBridgeTest {
-    public interface MyCallback{
-        void onTrigger(String arg);
-    }
     public static void start(){
-        JsbBridgeTest.myCallbackHashMap.put("requestLabelContent", arg ->{
+        JsJavaEventHandler.getInstance().addCallback("requestLabelContent", arg ->{
             System.out.print("@JAVA: here is the argument transport in" + arg);
             JsbBridge.sendToScript("changeLabelContent","Charlotte");
         });
-        JsbBridgeTest.myCallbackHashMap.put("requestLabelColor", arg ->{
+        JsJavaEventHandler.getInstance().addCallback("requestLabelColor", arg ->{
             System.out.print("@JAVA: here is the argument transport in" + arg);
             JsbBridge.sendToScript("changeLabelColor");
         });
-        JsbBridgeTest.myCallbackHashMap.put("requestBtnColor", arg ->{
+        JsJavaEventHandler.getInstance().addCallback("requestBtnColor", arg ->{
             System.out.print("@JAVA: here is the argument transport in" + arg);
             JsbBridge.sendToScript("changeLightColor");
         });
-
-        JsbBridge.setCallback(new JsbBridge.ICallback() {
-            @Override
-            public void onScript(String arg0, String arg1) {
-                JsbBridgeTest.myCallbackHashMap.get(arg0).onTrigger(arg1);
-            }
-        });
     }
-    public static HashMap<String, MyCallback> myCallbackHashMap = new HashMap<>();
-    private static JsbBridgeTest instance;
 }
