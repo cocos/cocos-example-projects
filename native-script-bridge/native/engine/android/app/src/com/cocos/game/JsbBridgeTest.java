ad6bd49d0fdb01ea2448ef3473e4a606d461d125
timlyeee <timelyeee@gmail.com>
2022-01-10 17:50:25 +0800

    update test case

--- native-script-bridge/native/engine/android/app/src/com/cocos/game/JsbBridgeTest.java
+++ native-script-bridge/native/engine/android/app/src/com/cocos/game/JsbBridgeTest.java
@@ -1,34 +1,27 @@
 //Script to add into com.cocos.game package, backup here
 package com.cocos.game;
-
 import com.cocos.lib.JsbBridgeWrapper;
-import com.cocos.lib.JsbBridge;
 
 public class JsbBridgeTest {
     public static void start(){
         //Original method
-        JsbBridgeWrapper.getInstance().addCallback("requestLabelContent", arg ->{
+        JsbBridgeWrapper jbw = JsbBridgeWrapper.getInstance();
+        jbw.addScriptEventListener("requestLabelContent", arg ->{
             System.out.print("@JAVA: here is the argument transport in" + arg);
-            JsbBridge.sendToScript("changeLabelContent","Charlotte");
+            jbw.dispatchEventToScript("changeLabelContent","Charlotte");
         });
-        JsbBridgeWrapper.getInstance().addCallback("requestLabelColor", arg ->{
+        jbw.addScriptEventListener("requestLabelColor", arg ->{
             System.out.print("@JAVA: here is the argument transport in" + arg);
-            JsbBridge.sendToScript("changeLabelColor");
+            jbw.dispatchEventToScript("changeLabelColor");
         });
-        JsbBridgeWrapper.getInstance().addCallback("requestBtnColor", arg ->{
+        jbw.addScriptEventListener("requestBtnColor", arg ->{
             System.out.print("@JAVA: here is the argument transport in" + arg);
-            JsbBridge.sendToScript("changeLightColor");
+            jbw.dispatchEventToScript("changeLightColor");
         });
 
         //Only use JavaEventHandler
-        JsbBridgeWrapper.getInstance().addCallback("generate100Callback", arg->{
-            AutoGenerator.generate100Callback();
-        });
-        JsbBridgeWrapper.getInstance().addCallback("dispatchJsEvent", arg->{
-            JsbBridgeWrapper.getInstance().dispatchScriptEvent("default");
-        });
-        JsbBridgeWrapper.getInstance().addCallback("removeNativeCallback", arg->{
-            AutoGenerator.releaseCallback();
+        jbw.addScriptEventListener("removeJSCallback", arg->{
+            jbw.removeAllListenersForEvent("requestBtnColor");
         });
 
     }
