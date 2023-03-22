e58b97c5ecb41de641009c6f51a0d4b4ee554d2b
Yiwen <15225434259xue@gmail.com>
2023-03-22 13:32:34 +0800

    optimize user script performance (#265)
    * optimize v8 performance for case-contact-data.scene
    
    * clean code

--- physics-3d/assets/common/scripts/ContactPointHelper.ts
+++ physics-3d/assets/common/scripts/ContactPointHelper.ts
@@ -1,55 +1,85 @@
-import { _decorator, Component, Node, Prefab, loader, instantiate, ICollisionEvent, ColliderComponent, game, Vec3, Quat } from 'cc';
+import { _decorator, Component, Node, Prefab, loader, instantiate, ICollisionEvent, ColliderComponent, game, Vec3, Quat, Pool, LabelComponent, CCInteger } from 'cc';
 const { ccclass, property, menu } = _decorator;
 const q_0 = new Quat();
+
+interface primitive {
+    pt: Node;
+    aw: Node;
+};
+
 @ccclass('COMMON.ContactPointHelper')
 @menu('common/ContactPointHelper')
 export class ContactPointHelper extends Component {
 
-    private static _point: Prefab = null!;
-    private static _arrow: Prefab = null!;
-    private static _flag = 0;
+    @property({ type: Prefab })
+    public point: Prefab = null as any;
+
+    @property({ type: Prefab })
+    public arrow: Prefab = null as any;
+
     private static _insArr: ContactPointHelper[] = [];
+
     private static _container: Node = null!;
+
+    private _pool: Pool<primitive> | null = null;
 
     private _entityMap: Map<string, { pt: Node[], aw: Node[] }> = new Map<string, { pt: Node[], aw: Node[] }>();
 
-    __preload () {
-        if (ContactPointHelper._point == null && !(ContactPointHelper._flag & 1)) {
-            ContactPointHelper._flag |= 1 << 0;
-            loader.loadRes('common/prefabs/Point', Prefab, (...args) => {
-                if (args) {
-                    if (args[0]) {
-                        console.error(args[0]);
-                    } else {
-                        ContactPointHelper._point = args[1] as Prefab;
-                    }
-                }
-            });
-        }
-        if (ContactPointHelper._arrow == null && !(ContactPointHelper._flag & (1 << 1))) {
-            ContactPointHelper._flag |= 1 << 1;
-            loader.loadRes('common/prefabs/Arrow', Prefab, (...args) => {
-                if (args) {
-                    if (args[0]) {
-                        console.error(args[0]);
-                    } else {
-                        ContactPointHelper._arrow = args[1] as Prefab;
-                    }
-                }
-            });
-        }
+    private originPoolSize = 4;
+
+    __preload() {
         if (ContactPointHelper._container == null) {
             ContactPointHelper._container = new Node('__CONTACT_POINT__');
             game.addPersistRootNode(ContactPointHelper._container);
         }
+
+        const originPoolSize = this.originPoolSize;
+
+        // init pool
+        this._pool = new Pool<primitive>(() => {
+            if (this.point == null || this.arrow == null) {
+                console.error('ContactPointHelper: point or arrow is null');
+            }
+
+            const pt = instantiate(this.point);
+            pt.setWorldScale(0.25, 0.25, 0.25);
+            pt.setParent(ContactPointHelper._container);
+
+            const aw = instantiate(this.arrow);
+            aw.setParent(ContactPointHelper._container);
+
+            pt.active = true;
+            aw.active = true;
+
+            const infFar = new Vec3(999999, 999999, 999999);
+            pt.setWorldPosition(infFar);
+            aw.setWorldPosition(infFar);
+
+            return { pt, aw };
+        }, originPoolSize);
+
+        const colliders = this.getComponents(ColliderComponent);
+
+        colliders.forEach((e: ColliderComponent) => {
+            if (!this._entityMap.has(e.uuid)) this._entityMap.set(e.uuid, { pt: [], aw: [] });
+            const map = this._entityMap.get(e.uuid)!;
+            const pts = map.pt;
+            const aws = map.aw;
+            for (let i = 0; i < originPoolSize; i++) {
+                const p = this._pool!.alloc();
+                pts.push(p.pt);
+                aws.push(p.aw);
+            }
+        });
     }
 
-    onLoad () {
+    onLoad() {
         ContactPointHelper._insArr.push(this);
     }
 
-    onEnable () {
+    onEnable() {
         const colliders = this.getComponents(ColliderComponent);
+
         const that = this;
         colliders.forEach((e: ColliderComponent) => {
             e.on('onCollisionEnter', that.onCollision, that);
@@ -58,7 +88,7 @@ export class ContactPointHelper extends Component {
         })
     }
 
-    onDisable () {
+    onDisable() {
         const colliders = this.getComponents(ColliderComponent);
         const that = this;
         colliders.forEach((e: ColliderComponent) => {
@@ -68,12 +98,12 @@ export class ContactPointHelper extends Component {
         })
     }
 
-    onCollision (event: ICollisionEvent) {
-        if (ContactPointHelper._point && ContactPointHelper._arrow) {
-            if (!this._entityMap.has(event.otherCollider.uuid)) this._entityMap.set(event.otherCollider.uuid, { pt: [], aw: [] });
-            const map = this._entityMap.get(event.otherCollider.uuid)!;
-            map.pt.forEach((e) => { e.active = false; });
-            map.aw.forEach((e) => { e.active = false; });
+    onCollision(event: ICollisionEvent) {
+        if (this.point && this.arrow) {
+            const map = this._entityMap.get(event.selfCollider.uuid)!;
+            const pts = map.pt;
+            const aws = map.aw;
+
             event.contacts.forEach((e, i) => {
                 const wp = new Vec3();
                 const wn = new Vec3();
@@ -86,29 +116,35 @@ export class ContactPointHelper extends Component {
                 }
 
                 let pt: Node, aw: Node;
+
                 if (map.pt.length > i) {
                     pt = map.pt[i]; aw = map.aw[i];
                     pt.active = true; aw.active = true;
                 } else {
-                    pt = instantiate(ContactPointHelper._point);
-                    pt.setWorldScale(0.25, 0.25, 0.25);
-                    pt.setParent(ContactPointHelper._container);
-
-                    aw = instantiate(ContactPointHelper._arrow);
-                    aw.setParent(ContactPointHelper._container);
-
+                    const marker = this._pool!.alloc();
+                    pt = marker.pt;
+                    aw = marker.aw;
+                    pt.active = true; aw.active = true;
                     map.pt.push(pt);
                     map.aw.push(aw);
                 }
+
                 pt.setWorldPosition(wp);
                 aw.setWorldPosition(wp);
                 Quat.rotationTo(q_0, Vec3.UNIT_Z, wn);
                 aw.setWorldRotation(q_0);
             })
+
+            const pool_size = pts.length;
+            const infFar = new Vec3(999999, 999999, 999999);
+            for (let i = event.contacts.length; i < pool_size; i++) {
+                pts[i].setWorldPosition(infFar);
+                aws[i].setWorldPosition(infFar);
+            }
         }
     }
 
-    onDestroy () {
+    onDestroy() {
         const index = ContactPointHelper._insArr.indexOf(this);
         if (index >= 0) {
             ContactPointHelper._insArr.splice(index, 1);
